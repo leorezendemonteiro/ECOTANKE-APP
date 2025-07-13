4edcc45f7e817043817eb1b344789c4836376960
leorezendemonteiro <leorezendemonteiro@gmail.com>
2025-07-13 19:12:03 -0300

    Delete firebaseconfig.js

--- firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "1:XXXXXX:web:XXXXXXXX"
-};
